c26eeb241fa339718fbd6d36571b34717fdedc04
hax0r31337 <liulihaocaiqwq@gmail.com>
2022-11-29 09:55:02 +0000

    better UX

--- app/src/main/java/dev/sora/protohax/MainActivity.kt
+++ app/src/main/java/dev/sora/protohax/MainActivity.kt
@@ -13,6 +13,7 @@ import com.github.megatronking.netbare.NetBareConfig
 import com.github.megatronking.netbare.NetBareListener
 import com.github.megatronking.netbare.ip.IpAddress
 import com.google.android.material.floatingactionbutton.FloatingActionButton
+import dev.sora.protohax.CacheManager.readString
 import dev.sora.protohax.CacheManager.readStringOrDefault
 import dev.sora.protohax.CacheManager.writeString
 
@@ -26,15 +27,6 @@ class MainActivity : Activity(), NetBareListener {
         .setSession("ProtoHax")
         .addRoute(IpAddress("0.0.0.0", 0))
 
-    private fun requestOverlayPermission(): Boolean {
-        if (!Settings.canDrawOverlays(this)) {
-            val myIntent = Intent(Settings.ACTION_MANAGE_OVERLAY_PERMISSION)
-            startActivity(myIntent)
-            return true
-        }
-        return false
-    }
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -42,30 +34,9 @@ class MainActivity : Activity(), NetBareListener {
         val button = findViewById<FloatingActionButton>(R.id.floatingBtn)
         val input = findViewById<TextView>(R.id.name_edit_text)
         button.setOnClickListener {
-            try {
-                if (requestOverlayPermission()) {
-                    Toast.makeText(this, getString(R.string.request_overlay), Toast.LENGTH_LONG).show()
-                    return@setOnClickListener
-                }
-                if (!NetBare.get().isActive) {
-                    val intent = NetBare.get().prepare()
-                    if (intent == null) {
-                        val targetPkgName = input.text.toString()
-                        writeString(TARGET_PACKAGE_CACHE_KEY, targetPkgName)
-                        NetBare.get().start(configBuilder
-                            .addAllowedApplication(targetPkgName)
-                            .build())
-                        Toast.makeText(this, getString(R.string.start_proxy_toast, targetPkgName), Toast.LENGTH_LONG).show()
-                    } else {
-                        this.startActivity(intent)
-                    }
-                } else {
-                    NetBare.get().stop()
-                    Toast.makeText(this, getString(R.string.stop_proxy_toast), Toast.LENGTH_LONG).show()
-                }
-            } catch (e: Throwable) {
-                Log.e("ProtoHax", "mitm", e)
-            }
+            val targetPkgName = input.text.toString()
+            writeString(TARGET_PACKAGE_CACHE_KEY, targetPkgName)
+            runMitMProxy(targetPkgName)
         }
         input.text = readStringOrDefault(TARGET_PACKAGE_CACHE_KEY, "com.mojang.minecraftpe")
 
@@ -98,7 +69,44 @@ class MainActivity : Activity(), NetBareListener {
         }
     }
 
+    private fun runMitMProxy(targetPkgName: String) {
+        try {
+            if (!Settings.canDrawOverlays(this)) {
+                Toast.makeText(this, getString(R.string.request_overlay), Toast.LENGTH_LONG).show()
+                val myIntent = Intent(Settings.ACTION_MANAGE_OVERLAY_PERMISSION)
+                this.startActivityForResult(myIntent, REQUEST_CODE_WITH_MITM_RECALL)
+                return
+            }
+            if (!NetBare.get().isActive) {
+                val intent = NetBare.get().prepare()
+                if (intent != null) {
+                    this.startActivityForResult(intent, REQUEST_CODE_WITH_MITM_RECALL_ONLY_OK)
+                    return
+                }
+                NetBare.get().start(configBuilder
+                    .addAllowedApplication(targetPkgName)
+                    .build())
+                Toast.makeText(this, getString(R.string.start_proxy_toast, targetPkgName), Toast.LENGTH_LONG).show()
+            } else {
+                NetBare.get().stop()
+                Toast.makeText(this, getString(R.string.stop_proxy_toast), Toast.LENGTH_LONG).show()
+            }
+        } catch (e: Throwable) {
+            Toast.makeText(this, e.toString(), Toast.LENGTH_LONG).show()
+            Log.e("ProtoHax", "mitm", e)
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        if (requestCode == REQUEST_CODE_WITH_MITM_RECALL
+            || (resultCode == RESULT_OK && requestCode == REQUEST_CODE_WITH_MITM_RECALL_ONLY_OK)) {
+            runMitMProxy(readString(TARGET_PACKAGE_CACHE_KEY) ?: return)
+        }
+    }
+
     companion object {
         private const val TARGET_PACKAGE_CACHE_KEY = "TARGET_PACKAGE"
+        private const val REQUEST_CODE_WITH_MITM_RECALL = 1
+        private const val REQUEST_CODE_WITH_MITM_RECALL_ONLY_OK = 1
     }
 }
