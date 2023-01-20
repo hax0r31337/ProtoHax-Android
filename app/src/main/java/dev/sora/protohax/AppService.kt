3a3baa2d324a25126ab4a42d7bf7398ce5e6f47a
hax0r31337 <liulihaocaiqwq@gmail.com>
2023-01-20 05:29:59 +0000

    use libmitm

--- app/src/main/java/dev/sora/protohax/AppService.kt
+++ app/src/main/java/dev/sora/protohax/AppService.kt
@@ -3,31 +3,142 @@ package dev.sora.protohax
 import android.app.*
 import android.content.Intent
 import android.graphics.BitmapFactory
+import android.net.VpnService
+import android.os.ParcelFileDescriptor
+import android.util.Log
 import android.view.WindowManager
 import androidx.core.app.NotificationCompat
-import com.github.megatronking.netbare.NetBare
-import com.github.megatronking.netbare.NetBareService
+import dev.sora.protohax.ContextUtils.readString
 import dev.sora.protohax.ContextUtils.toast
 import dev.sora.protohax.forwarder.R
+import libmitm.Libmitm
+import libmitm.Protector
+import libmitm.Redirector
+import libmitm.TUN
+import java.net.Inet4Address
+import java.net.Inet6Address
+import java.net.NetworkInterface
 import kotlin.random.Random
 
 
-class AppService : NetBareService() {
+class AppService : VpnService(), Protector {
 
-    private lateinit var windowManager: WindowManager
+    private var vpnDescriptor: ParcelFileDescriptor? = null
+    private var tun: TUN? = null
 
     override fun onCreate() {
         val notificationManager = getSystemService(Service.NOTIFICATION_SERVICE) as NotificationManager
         if (notificationManager.getNotificationChannel(CHANNEL_ID) == null) {
             notificationManager.createNotificationChannel(NotificationChannel(CHANNEL_ID, getString(R.string.app_name), NotificationManager.IMPORTANCE_LOW))
         }
-
-        windowManager = getSystemService(WINDOW_SERVICE) as WindowManager
     }
 
-    override fun notificationId() = Random.Default.nextInt()
+    override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
+        intent ?: return START_NOT_STICKY
 
-    override fun createNotification(): Notification {
+        val action = intent.action
+        try {
+            if (ACTION_START == action) {
+                startVPN()
+                startForeground(1, createNotification())
+            } else if (ACTION_STOP == action) {
+                stopVPN()
+                stopForeground(true)
+                stopSelf()
+            } else {
+                stopSelf()
+            }
+        } catch (t: Throwable) {
+            Log.e("ProtoHax", "command", t)
+        }
+        return super.onStartCommand(intent, flags, startId)
+    }
+
+    private fun startVPN() {
+        val (hasIPv4, hasIPv6) = checkNetState()
+
+        val builder = Builder()
+        builder.setBlocking(true)
+        builder.setMtu(VPN_MTU)
+        builder.setSession("ProtoHax")
+        builder.addAllowedApplication(readString(MainActivity.KEY_TARGET_PACKAGE_CACHE)!!)
+        builder.addAllowedApplication(this.packageName)
+//        builder.addDnsServer("8.8.8.8")
+        // ipv4
+        if (hasIPv4) {
+            builder.addAddress(PRIVATE_VLAN4_CLIENT, 30)
+            builder.addRoute("0.0.0.0", 0)
+        }
+        // ipv6
+        if (hasIPv6) {
+            builder.addAddress(PRIVATE_VLAN6_CLIENT, 126)
+            builder.addRoute("::", 0)
+        }
+
+        val vpnDescriptor = builder.establish() ?: return
+        this.vpnDescriptor = vpnDescriptor
+
+        val tun = TUN().apply {
+            fileDescriber = vpnDescriptor.fd
+            mtu = VPN_MTU
+            iPv6Config = when {
+                hasIPv4 && hasIPv6 -> Libmitm.IPv6Enable
+                hasIPv4 -> Libmitm.IPv6Disable
+                hasIPv6 -> Libmitm.IPv6Only
+                else -> error("invalid state")
+            }
+            fdProtector = this@AppService
+            if (hasIPv4) {
+                addLocalIP(PRIVATE_VLAN4_CLIENT)
+            }
+            if (hasIPv6) {
+                addLocalIP(PRIVATE_VLAN6_CLIENT)
+            }
+            val forward = readString(MainActivity.KEY_TARGET_PACKET_FORWARD)!!
+            udpRedirector = Redirector { _, _, _, _ -> forward }
+        }
+        this.tun = tun
+        tun.start()
+        Log.i("ProtoHax", "netstack started")
+        isActive = true
+        instance = this
+        try {
+            serviceListeners.forEach { it.onServiceStarted() }
+        } catch (t: Throwable) {
+            Log.e("ProtoHax", "start callback", t)
+        }
+    }
+
+    private fun stopVPN() {
+        isActive = false
+        try {
+            serviceListeners.forEach { it.onServiceStopped() }
+        } catch (t: Throwable) {
+            Log.e("ProtoHax", "stop callback", t)
+        }
+        tun?.close()
+        vpnDescriptor?.close()
+    }
+
+    private fun checkNetState(): Pair<Boolean, Boolean> {
+        var hasIPv4 = false
+        var hasIPv6 = false
+
+        val networkInterfaces = NetworkInterface.getNetworkInterfaces()
+        while (networkInterfaces.hasMoreElements()) {
+            val ni = networkInterfaces.nextElement()
+            for (addr in ni.interfaceAddresses) {
+                if (addr.address is Inet6Address) {
+                    hasIPv6 = true
+                } else if (addr.address is Inet4Address) {
+                    hasIPv4 = true
+                }
+            }
+        }
+        return hasIPv4 to hasIPv6
+    }
+
+    private fun createNotification(): Notification {
         val flag = PendingIntent.FLAG_IMMUTABLE or PendingIntent.FLAG_UPDATE_CURRENT
 
         val intent = Intent(this, MainActivity::class.java)
@@ -35,31 +146,37 @@ class AppService : NetBareService() {
         intent.action = Intent.ACTION_MAIN
         val pendingIntent = PendingIntent.getActivity(this, 0, intent, flag)
 
-//        val stopIntent = Intent(ACTION_STOP)
-//        stopIntent.setPackage(packageName)
-//        val pendingIntent1 = PendingIntent.getActivity(this, 1, stopIntent, flag)
-
         val builder = NotificationCompat.Builder(this, CHANNEL_ID)
-
-        val seq = AntiModification.call
-        if (AntiModification.validateAppSignature(this).let { !it.first || !it.second.startsWith("fuck") } || seq == AntiModification.call || seq + 1 == AntiModification.call) {
-            toast("Internal error occurred, please contact the developer.")
-            throw NullPointerException("null")
-        } else {
-            builder
-                .setContentTitle(getString(R.string.app_name))
-                .setContentText(getString(R.string.proxy_notification, getString(R.string.app_name), NetBare.get().config.allowedApplications.firstOrNull() ?: "unknown"))
-                .setSmallIcon(R.drawable.notification_icon)
-                .setLargeIcon(BitmapFactory.decodeResource(resources, R.mipmap.ic_launcher))
-                .setOngoing(true)
-                .setContentIntent(pendingIntent)
-//                .addAction(R.drawable.notification_icon, getString(R.string.stop_proxy_notify), pendingIntent1)
-        }
+            .setContentTitle(getString(R.string.app_name))
+            .setContentText(getString(R.string.proxy_notification, getString(R.string.app_name), readString(MainActivity.KEY_TARGET_PACKAGE_CACHE)!!))
+            .setSmallIcon(R.drawable.notification_icon)
+            .setLargeIcon(BitmapFactory.decodeResource(resources, R.mipmap.ic_launcher))
+            .setOngoing(true)
+            .setContentIntent(pendingIntent)
 
         return builder.build()
     }
 
     companion object {
+        const val ACTION_START = "dev.sora.libmitm1.vpn.start"
+        const val ACTION_STOP = "dev.sora.libmitm1.vpn.stop"
         const val CHANNEL_ID = "dev.sora.protohax.NOTIFICATION_CHANNEL_ID"
+
+        const val VPN_MTU = 1500
+        const val PRIVATE_VLAN4_CLIENT = "26.26.26.1"
+        const val PRIVATE_VLAN6_CLIENT = "da26:2626::1"
+
+        lateinit var instance: AppService
+            private set
+        var isActive = false
+        private val serviceListeners = mutableSetOf<ServiceListener>()
+
+        fun addListener(listener: ServiceListener) {
+            serviceListeners.add(listener)
+        }
+
+        fun removeListener(listener: ServiceListener) {
+            serviceListeners.remove(listener)
+        }
     }
 }
--- app/src/main/java/dev/sora/protohax/MainActivity.kt
+++ app/src/main/java/dev/sora/protohax/MainActivity.kt
@@ -6,16 +6,10 @@ import android.content.Intent
 import android.content.pm.ApplicationInfo
 import android.content.pm.PackageManager
 import android.content.res.ColorStateList
+import android.net.VpnService
 import android.os.Bundle
 import android.util.Log
-import android.widget.Button
 import android.widget.TextView
-import com.github.megatronking.netbare.NetBare
-import com.github.megatronking.netbare.NetBareConfig
-import com.github.megatronking.netbare.NetBareListener
-import com.github.megatronking.netbare.NetBareUtils
-import com.github.megatronking.netbare.ip.IpAddress
-import com.github.megatronking.netbare.proxy.UdpProxyServerForwarder
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 import dev.sora.protohax.ContextUtils.hasInternetPermission
 import dev.sora.protohax.ContextUtils.isAppExists
@@ -23,18 +17,10 @@ import dev.sora.protohax.ContextUtils.readString
 import dev.sora.protohax.ContextUtils.readStringOrDefault
 import dev.sora.protohax.ContextUtils.toast
 import dev.sora.protohax.ContextUtils.writeString
-import dev.sora.protohax.activity.LogcatActivity
 import dev.sora.protohax.forwarder.R
 
 
-class MainActivity : Activity(), NetBareListener {
-
-    private val configBuilder: NetBareConfig.Builder
-        get() = NetBareConfig.Builder()
-        .setMtu(4096)
-        .setAddress(IpAddress("10.1.10.1", 32))
-        .setSession("ProtoHax_Forwarder")
-        .addRoute(IpAddress("0.0.0.0", 0))
+class MainActivity : Activity(), ServiceListener {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -51,7 +37,7 @@ class MainActivity : Activity(), NetBareListener {
             }
             writeString(KEY_TARGET_PACKAGE_CACHE, targetPkgName)
             writeString(KEY_TARGET_PACKET_FORWARD, input1.text.toString())
-            runMitMProxy(targetPkgName)
+            runMitMProxy()
         }
         input.text = readStringOrDefault(KEY_TARGET_PACKAGE_CACHE, "com.mojang.minecraftpe")
         input.setOnLongClickListener {
@@ -60,12 +46,8 @@ class MainActivity : Activity(), NetBareListener {
         }
         input1.text = readStringOrDefault(KEY_TARGET_PACKET_FORWARD, "192.168.2.1:19132")
 
-        NetBare.get().registerNetBareListener(this)
+        AppService.addListener(this)
         updateConnStatus()
-        findViewById<Button>(R.id.button_show_logs).setOnClickListener {
-            val myIntent = Intent(this, LogcatActivity::class.java)
-            this.startActivity(myIntent)
-        }
     }
 
     private fun appChooser() {
@@ -90,10 +72,10 @@ class MainActivity : Activity(), NetBareListener {
 
     override fun onDestroy() {
         super.onDestroy()
-        NetBare.get().unregisterNetBareListener(this)
+        AppService.removeListener(this)
     }
 
-    private fun updateConnStatus(status: Boolean = NetBare.get().isActive) {
+    private fun updateConnStatus(status: Boolean = AppService.isActive) {
         val button = findViewById<FloatingActionButton>(R.id.floating_button)
         button.backgroundTintList = ColorStateList.valueOf(getColor(if (status) R.color.actionbtn_active else R.color.actionbtn_inactive))
 
@@ -117,23 +99,19 @@ class MainActivity : Activity(), NetBareListener {
         }
     }
 
-    private fun runMitMProxy(targetPkgName: String) {
+    private fun runMitMProxy() {
         try {
-            if (!NetBare.get().isActive) {
-                val intent = NetBare.get().prepare()
+            if (!AppService.isActive) {
+                val intent = VpnService.prepare(this)
                 if (intent != null) {
                     this.startActivityForResult(intent, REQUEST_CODE_WITH_MITM_RECALL_ONLY_OK)
                     return
                 }
                 val forward = readString(KEY_TARGET_PACKET_FORWARD)!!.split(":")
-                UdpProxyServerForwarder.targetForwardIp = NetBareUtils.convertIp(forward[0])
-                UdpProxyServerForwarder.targetForwardPort = NetBareUtils.convertPort(forward[1].toInt())
-                NetBare.get().start(configBuilder
-                    .addAllowedApplication(targetPkgName)
-                    .build())
-                toast(getString(R.string.start_proxy_toast, targetPkgName))
+                emitMessage(AppService.ACTION_START)
+                toast(getString(R.string.start_proxy_toast, readString(KEY_TARGET_PACKAGE_CACHE)))
             } else {
-                NetBare.get().stop()
+                emitMessage(AppService.ACTION_STOP)
                 toast(R.string.stop_proxy_toast)
             }
         } catch (e: Throwable) {
@@ -142,16 +120,22 @@ class MainActivity : Activity(), NetBareListener {
         }
     }
 
+    private fun emitMessage(msg: String) {
+        val intent = Intent(msg)
+        intent.setPackage(packageName)
+        startForegroundService(intent)
+    }
+
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         if (requestCode == REQUEST_CODE_WITH_MITM_RECALL
             || (resultCode == RESULT_OK && requestCode == REQUEST_CODE_WITH_MITM_RECALL_ONLY_OK)) {
-            runMitMProxy(readString(KEY_TARGET_PACKAGE_CACHE) ?: return)
+            runMitMProxy()
         }
     }
 
     companion object {
-        private const val KEY_TARGET_PACKAGE_CACHE = "TARGET_PACKAGE"
-        private const val KEY_TARGET_PACKET_FORWARD = "TARGET_FORWARD"
+        const val KEY_TARGET_PACKAGE_CACHE = "TARGET_PACKAGE"
+        const val KEY_TARGET_PACKET_FORWARD = "TARGET_FORWARD"
         private const val REQUEST_CODE_WITH_MITM_RECALL = 0
         private const val REQUEST_CODE_WITH_MITM_RECALL_ONLY_OK = 1
     }
