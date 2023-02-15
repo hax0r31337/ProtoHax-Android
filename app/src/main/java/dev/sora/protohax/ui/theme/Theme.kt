2402bc94ef23fec206bd1740064efded0d8cb0d6
hax0r31337 <liulihaocaiqwq@gmail.com>
2023-02-15 15:07:06 +0000

    ux improvements

--- app/src/main/java/dev/sora/protohax/ui/theme/Theme.kt
+++ app/src/main/java/dev/sora/protohax/ui/theme/Theme.kt
@@ -1,12 +1,15 @@
 package dev.sora.protohax.ui.theme
 
+import android.app.Activity
 import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.SideEffect
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalView
+import androidx.core.view.WindowCompat
 import com.google.accompanist.systemuicontroller.rememberSystemUiController
 
 @Composable
@@ -27,6 +30,12 @@ fun MyApplicationTheme(
     val view = LocalView.current
     if (!view.isInEditMode) {
         rememberSystemUiController().setSystemBarsColor(Color.Transparent)
+        if (view.context is Activity) {
+            SideEffect {
+                val window = (view.context as Activity).window
+                WindowCompat.getInsetsController(window, view).isAppearanceLightStatusBars = !darkTheme
+            }
+        }
     }
 
     MaterialTheme(
