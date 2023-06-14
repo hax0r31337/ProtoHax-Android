076b9e47c831f5b72d4e2c70ba3e2e03c9f04a1e
hax0r31337 <liulihaocaiqwq@gmail.com>
2023-06-14 06:52:23 +0000

    feat: dynamic color for shortcut

--- app/src/main/java/dev/sora/protohax/ui/overlay/Shortcut.kt
+++ app/src/main/java/dev/sora/protohax/ui/overlay/Shortcut.kt
@@ -1,17 +1,20 @@
 package dev.sora.protohax.ui.overlay
 
+import android.content.Context
+import android.content.res.Configuration
+import android.graphics.Color
 import android.graphics.PixelFormat
 import android.graphics.drawable.GradientDrawable
+import android.os.Build
 import android.view.Gravity
 import android.view.WindowManager
 import android.widget.TextView
 import dev.sora.protohax.relay.MinecraftRelay
-import dev.sora.protohax.relay.gui.SelectionMenu
 import dev.sora.relay.cheat.module.CheatModule
 import dev.sora.relay.cheat.module.EventModuleToggle
 import dev.sora.relay.game.event.EventHook
 
-class Shortcut(val module: CheatModule, val overlayManager: OverlayManager) {
+class Shortcut(val module: CheatModule, private val overlayManager: OverlayManager) {
 
 	val params = WindowManager.LayoutParams(
 		WindowManager.LayoutParams.WRAP_CONTENT,
@@ -47,7 +50,7 @@ class Shortcut(val module: CheatModule, val overlayManager: OverlayManager) {
 			setPadding(30, 30, 30, 30)
 
 			background = GradientDrawable().apply {
-				setColor(SelectionMenu.BACKGROUND_COLOR)
+				setColor(getBackgroundColor(ctx))
 				cornerRadius = 15f
 				alpha = 150
 			}
@@ -74,6 +77,45 @@ class Shortcut(val module: CheatModule, val overlayManager: OverlayManager) {
 	}
 
 	private fun TextView.updateTextColor(targetState: Boolean = module.state) {
-		setTextColor(if (module.canToggle) if (targetState) SelectionMenu.TOGGLE_ON_COLOR_RGB else SelectionMenu.TOGGLE_OFF_COLOR_RGB else SelectionMenu.TEXT_COLOR)
+		val ctx = overlayManager.ctx
+		setTextColor(if (module.canToggle) if (targetState) getToggleOnColor(ctx) else getToggleOffColor(ctx) else getTextColor(ctx))
+	}
+
+	/**
+	 * color used if dynamic color is not available
+	 */
+	companion object {
+		private val TEXT_COLOR = Color.parseColor("#c1c1c1")
+		private val BACKGROUND_COLOR = Color.parseColor("#1b1b1b")
+		private val TOGGLE_ON_COLOR = Color.parseColor("#00a93f")
+		private val TOGGLE_OFF_COLOR = Color.parseColor("#c81000")
+
+		private fun Context.isNightMode(): Boolean {
+			return resources.configuration.uiMode and Configuration.UI_MODE_NIGHT_MASK == Configuration.UI_MODE_NIGHT_YES
+		}
+
+		private fun getTextColor(ctx: Context): Int {
+			return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
+				if (ctx.isNightMode()) ctx.getColor(android.R.color.system_neutral1_100) else ctx.getColor(android.R.color.system_neutral1_900)
+			} else TEXT_COLOR
+		}
+
+		private fun getBackgroundColor(ctx: Context): Int {
+			return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
+				if (ctx.isNightMode()) ctx.getColor(android.R.color.system_neutral1_900) else ctx.getColor(android.R.color.system_neutral1_10)
+			} else BACKGROUND_COLOR
+		}
+
+		private fun getToggleOnColor(ctx: Context): Int {
+			return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
+				if (ctx.isNightMode()) ctx.getColor(android.R.color.system_accent1_200) else ctx.getColor(android.R.color.system_accent1_600)
+			} else TOGGLE_ON_COLOR
+		}
+
+		private fun getToggleOffColor(ctx: Context): Int {
+			return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
+				if (ctx.isNightMode()) ctx.getColor(android.R.color.system_accent1_600) else ctx.getColor(android.R.color.system_accent1_200)
+			} else TOGGLE_OFF_COLOR
+		}
 	}
 }
